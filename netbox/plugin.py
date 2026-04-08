264f870b2223f082ddf272b9eec1d505b61fab8c
João Vitor Caetano <95577389+Caetanojao@users.noreply.github.com>
2026-04-08 00:25:37 -0300

    Add plugin configuration for nbxsync

---
+++ netbox/plugin.py
@@ -0,0 +1,10 @@
+PLUGINS = ["nbxsync"]
+
+PLUGINS_CONFIG = {
+    "nbxsync": {
+        "zabbix_url": "http://192.168.15.13:8080",
+        "zabbix_user": "Admin",
+        "zabbix_password": "zabbix",
+        "verify_ssl": False,
+    }
+}
