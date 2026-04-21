2fd1b24218df8ad573d1c4398fc1e278f28d5dfc
João Vitor Caetano <95577389+Caetanojao@users.noreply.github.com>
2026-04-21 15:12:34 -0300

    Update Zabbix URL in plugin configuration

--- netbox/plugin.py
+++ netbox/plugin.py
@@ -2,7 +2,7 @@ PLUGINS = ["nbxsync"]
 
 PLUGINS_CONFIG = {
     "nbxsync": {
-        "zabbix_url": "http://192.168.15.13:8080",
+        "zabbix_url": "http://IP_DA_VM:8080",
         "zabbix_user": "Admin",
         "zabbix_password": "zabbix",
         "verify_ssl": False,
